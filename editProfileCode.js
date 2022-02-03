52c34a5ce2df862f8907ac2bd32bb1c639613a52
Tejus Kabadi <tejuskabadi@Tejuss-MacBook-Pro.local>
2022-02-03 05:45:52 +0530

    made success lottie show and trigger

--- editProfileCode.js
+++ editProfileCode.js
@@ -168,6 +168,7 @@ MemberStack.onReady.then(function (member) {
 
       function successFuncp(data) {
         $("#updateLoading").css("opacity", "0");
+        $("#updateSuccess").css("display", "block");
         $("#updateSuccess").trigger("click");
         console.log(data);
       }
