274f1f18e9a3804788190142318c9a6e43e74f61
Tejus Kabadi <tejuskabadi@Tejuss-MacBook-Pro.local>
2022-02-09 19:44:59 +0530

    changed to javascript event listener

--- editProfileCode.js
+++ editProfileCode.js
@@ -202,8 +202,8 @@ MemberStack.onReady.then(function (member) {
         $("#Public-phone").attr("disabled", "disabled");
         $("#Public-email").attr("disabled", "disabled");
 
-        $("#Public-phone").mouseenter(upgradeToElite());
-        $("#Public-email").mouseenter(upgradeToElite());
+        $("#Public-phone").addEventListener("mouseover", upgradeToElite());
+        $("#Public-email").addEventListener("mouseover", upgradeToElite());
       }
 
       if (membership.name == "Free Plan") {
