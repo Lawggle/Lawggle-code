6a36da1d072103f757b491197385d95f93b3cf1f
Tejus Kabadi <tejuskabadi@Tejuss-MacBook-Pro.local>
2022-02-17 01:36:44 +0530

    getting leads

--- leadsPage.js
+++ leadsPage.js
@@ -1,3 +1,10 @@
 // Adding the leads page
 
-console.log("Leads added");
+fetch("https://sheet.best/api/sheets/c537b30c-6a62-49e9-bbb7-913b076eee99/tabs/Leads")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("data is", data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
