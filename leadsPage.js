689975f06e6f0973fa8ea6ddd05cab243fdbec1a
Tejus Kabadi <tejuskabadi@Tejuss-MacBook-Pro.local>
2022-02-17 03:24:53 +0530

    show more logic

--- leadsPage.js
+++ leadsPage.js
@@ -1,23 +1,29 @@
-// Adding the leads page
-
 MemberStack.onReady.then(function (member) {
+  // Get membership ID from memberstack
   var mID = member["id"];
 
+  //   Get the sample row and row container
   var templateRow = document.querySelector(".row-template .table-row");
   const rowsWrapper = document.querySelector(".table-data-wrapper");
 
-  console.log("templateRow", templateRow);
-  console.log("rows wrapper", rowsWrapper);
-
+  //   Make API call to google sheet and get data
   fetch(`https://sheet.best/api/sheets/c537b30c-6a62-49e9-bbb7-913b076eee99/tabs/Leads/MID/${mID}`)
     .then((response) => response.json())
     .then((data) => {
       console.log("data is", data);
 
+      //   Run loop on returned results and render inside row container
       data.forEach((row, index) => {
+        //   Create copy of sample row
         const newRow = templateRow.cloneNode(true);
-
+        //   Replace data with row from sheet
         newRow.querySelector(".data-number").innerHTML = index;
+
+        // Adding event listener on show more button
+        newRow.querySelector(".show-more").onclick = function () {
+          newRow.querySelector(".message-min").style.display = "none";
+          newRow.querySelector(".message-max").style.display = "block";
+        };
 
         rowsWrapper.appendChild(newRow);
       });
