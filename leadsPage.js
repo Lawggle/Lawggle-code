080aa6895b0535bd706a0efd28428823af7b6582
Tejus Kabadi <tejuskabadi@Tejuss-MacBook-Pro.local>
2022-02-17 03:04:26 +0530

    moved code within fetch call

--- leadsPage.js
+++ leadsPage.js
@@ -3,23 +3,23 @@
 MemberStack.onReady.then(function (member) {
   var mID = member["id"];
 
-  fetch(`https://sheet.best/api/sheets/c537b30c-6a62-49e9-bbb7-913b076eee99/tabs/Leads/MID/${mID}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("data is", data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-
   var templateRow = $(".row-template").firstElementChild;
   const rowsWrapper = $(".table-data-wraper");
 
   console.log("templateRow", templateRow);
   console.log("rows wrapper", rowsWrapper);
 
-  data.forEach((row, index) => {
-    const newRow = templateRow.cloneNode(true);
-    rowsWrapper.appendChild(newRow);
-  });
+  fetch(`https://sheet.best/api/sheets/c537b30c-6a62-49e9-bbb7-913b076eee99/tabs/Leads/MID/${mID}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("data is", data);
+
+      data.forEach((row, index) => {
+        const newRow = templateRow.cloneNode(true);
+        rowsWrapper.appendChild(newRow);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
