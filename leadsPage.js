aa615aaa067059f2fe210a9611a422b3acab9d51
Tejus Kabadi <tejuskabadi@Tejuss-MacBook-Pro.local>
2022-02-17 05:28:53 +0530

    hiding row instead of deleting

--- leadsPage.js
+++ leadsPage.js
@@ -18,44 +18,49 @@ MemberStack.onReady.then(function (member) {
 
       //   Run loop on returned results and render inside row container
       data.forEach((row, index) => {
-        //   Create copy of sample row
-        const newRow = templateRow.cloneNode(true);
+        // Only render if row is not deleted ie hidden
+        if (!row.Hide) {
+          //   Create copy of sample row
+          const newRow = templateRow.cloneNode(true);
 
-        //   Replace data with row from sheet
-        newRow.querySelector(".data-number").innerHTML = index + 1;
-        newRow.querySelector(".data-name").innerHTML = row["Pro Name"];
-        newRow.querySelector(".data-date").innerHTML = row["Date Submitted"];
-        newRow.querySelector(".data-email").innerHTML = row["Sender Email"];
-        newRow.querySelector(".message-min").innerHTML = row["Message"].substring(0, 25) + "...";
-        newRow.querySelector(".message-max").innerHTML = row["Message"];
+          //   Replace data with row from sheet
+          newRow.querySelector(".data-number").innerHTML = index + 1;
+          newRow.querySelector(".data-name").innerHTML = row["Pro Name"];
+          newRow.querySelector(".data-date").innerHTML = row["Date Submitted"];
+          newRow.querySelector(".data-email").innerHTML = row["Sender Email"];
+          newRow.querySelector(".message-min").innerHTML = row["Message"].substring(0, 25) + "...";
+          newRow.querySelector(".message-max").innerHTML = row["Message"];
 
-        // Adding event listener on show more button
-        newRow.querySelector(".show-more").onclick = function () {
-          newRow.querySelector(".message-min").style.display = "none";
-          newRow.querySelector(".message-max").style.display = "block";
-          this.style.display = "none";
-        };
+          // Adding event listener on show more button
+          newRow.querySelector(".show-more").onclick = function () {
+            newRow.querySelector(".message-min").style.display = "none";
+            newRow.querySelector(".message-max").style.display = "block";
+            this.style.display = "none";
+          };
 
-        // Adding event listener to delete button
-        newRow.querySelector(".delete").onclick = function () {
-          newRow.style.display = "none";
+          // Adding event listener to delete button
+          newRow.querySelector(".delete").onclick = function () {
+            newRow.style.display = "none";
 
-          fetch(`https://sheet.best/api/sheets/c537b30c-6a62-49e9-bbb7-913b076eee99/tabs/Leads/UUID/${row["UUID"]}`, {
-            method: "DELETE",
-          })
-            .then((response) => response.json())
-            .then((data) => {
-              console.log("row deleted successfully");
-              console.log(data);
+            fetch(`https://sheet.best/api/sheets/c537b30c-6a62-49e9-bbb7-913b076eee99/tabs/Leads/UUID/${row["UUID"]}`, {
+              method: "PATCH",
+              mode: "cors",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                Hide: "true",
+              }),
             })
-            .catch((error) => {
-              console.error(error);
-            });
-        };
+              .then((r) => r.json())
+              .then(console.log)
+              .catch(console.error);
+          };
 
-        // Showing the sample row
-        newRow.style.display = "flex";
-        rowsWrapper.appendChild(newRow);
+          // Showing the sample row
+          newRow.style.display = "flex";
+          rowsWrapper.appendChild(newRow);
+        }
       });
     })
     .catch((error) => {
