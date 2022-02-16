918ddc344ae2fc9aaabe841db654217bd779acbb
Tejus Kabadi <tejuskabadi@Tejuss-MacBook-Pro.local>
2022-02-17 01:43:46 +0530

    filtering with MID

--- leadsPage.js
+++ leadsPage.js
@@ -1,10 +1,14 @@
 // Adding the leads page
 
-fetch("https://sheet.best/api/sheets/c537b30c-6a62-49e9-bbb7-913b076eee99/tabs/Leads")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log("data is", data);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+MemberStack.onReady.then(function (member) {
+  var mID = member["id"];
+
+  fetch(`https://sheet.best/api/sheets/c537b30c-6a62-49e9-bbb7-913b076eee99/tabs/Leads/MID/${mID}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("data is", data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
