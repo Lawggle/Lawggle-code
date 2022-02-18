2bb5d1af6822a99ea02c40adc0653df7b070e374
Tejus Kabadi <tejuskabadi@Tejuss-MacBook-Pro.local>
2022-02-19 01:38:21 +0530

    not hiding show button

--- leadsPage.js
+++ leadsPage.js
@@ -37,7 +37,6 @@ MemberStack.onReady.then(function (member) {
           newRow.querySelector(".show-more").onclick = function () {
             newRow.querySelector(".message-min").style.display = "none";
             newRow.querySelector(".message-max").style.display = "block";
-            this.style.display = "none";
           };
 
           // Adding event listener to delete button
