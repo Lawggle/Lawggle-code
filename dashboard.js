4c84f4b3ca650bf6b9cf72395ed6f3c502a51309
Lawggle <94259820+Lawggle@users.noreply.github.com>
2022-04-19 11:01:51 -0700

    adding name to datalayer

--- dashboard.js
+++ dashboard.js
@@ -6,15 +6,14 @@ MemberStack.onReady.then(function (member) {
   var mPlan = membership.name;
   var mEmail = member["email"];
   var mPhone = member["phone"];
-  var mName =  member["first-name"];
-  var mlName =  member["last-name"];
+  var mName =  member["first-name"] + " " + member["last-name"];
   
   //adding details to the datalayer
   window.dataLayer = window.dataLayer || [];
   window.dataLayer.push({
    'event': 'login',
    'member_id': mID,
-   'member_name':  
+   'member_name': mName,
 });
   
   $("#mPlan").text(mPlan);
