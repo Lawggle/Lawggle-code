b258ec5ffe9af852fa5e4e3f038369e923cb7528
Lawggle <94259820+Lawggle@users.noreply.github.com>
2024-03-03 19:34:21 -0800

    Update profileCode.js
    changing sheet.best connection URL

--- profileCode.js
+++ profileCode.js
@@ -87,7 +87,7 @@ var getUrlParameter = function getUrlParameter(sParam) {
 };
 
 var profile = getUrlParameter("profile");
-var url = "https://sheet.best/api/sheets/c537b30c-6a62-49e9-bbb7-913b076eee99/Alias/" + profile;
+var url = "https://sheet.best/api/sheets/87c02226-b9fc-4f91-a018-ce0fca93e9aa/Alias/" + profile;
 
 $(".profile-card").css("opacity", "0%");
 $(".profile-details").css("display", "none");
